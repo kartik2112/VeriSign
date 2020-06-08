778f9eb0f7cf50ad607481a93a4814f39b9392bb
= <kartik.shenoyy@gmail.com>
2020-06-08 22:45:03 +0530

    Minor changes made to angular app

--- Angular App/SignatureVerify/src/app/app.component.ts
+++ Angular App/SignatureVerify/src/app/app.component.ts
@@ -8,14 +8,11 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent  {
-  // files: any[] = [];
   file1:any;
   file2:any;
   public imagePath;
   imgURL1: any;
   imgURL2: any;
-  // public message1: string;
-  // public message2: string;
   waitingResponse = false;
   verifySignResult = -1;
   conf: number = 0;
@@ -34,60 +31,6 @@ export class AppComponent  {
   fileBrowseHandler(file,imgNo) {
     this.prepareFile(file,imgNo);
   }
-
-  /**
-  //  * Delete file from files list
-  //  * @param index (File index)
-  //  */
-  // deleteFile(index: number) {
-  //   this.files.splice(index, 1);
-  // }
-
-  /**
-   * Simulate the upload process
-   */
-  // uploadFilesSimulator(index: number) {
-  //   setTimeout(() => {
-  //     if (index === this.files.length) {
-  //       return;
-  //     } else {
-  //       const progressInterval = setInterval(() => {
-  //         if (this.files[index].progress === 100) {
-  //           clearInterval(progressInterval);
-  //           this.uploadFilesSimulator(index + 1);
-  //         } else {
-  //           this.files[index].progress += 5;
-  //         }
-  //       }, 200);
-  //     }
-  //   }, 1000);
-  // }
-
-  // /**
-  //  * Convert Files list to normal array list
-  //  * @param files (Files List)
-  //  */
-  // prepareFilesList(files: Array<any>) {
-  //   for (const item of files) {
-  //     item.progress = 0;
-  //     this.files.push(item);
-  //   }
-
-  //   var mimeType = files[0].type;
-  //   if (mimeType.match(/image\/*/) == null) {
-  //     this.message1 = "Only images are supported.";
-  //     return;
-  //   }
- 
-  //   var reader = new FileReader();
-  //   this.imagePath = files;
-  //   reader.readAsDataURL(files[0]); 
-  //   reader.onload = (_event) => { 
-  //     this.imgURL1 = reader.result; 
-  //   }
-
-  //   this.uploadFilesSimulator(0);
-  // }
 
   prepareFile(files: Array<any>,imgNo) {
     console.log(!this.imgURL1 || !this.imgURL2)
@@ -119,25 +62,8 @@ export class AppComponent  {
     );
  
     
-
-    // this.uploadFilesSimulator(0);
   }
 
-  /**
-   * format bytes
-   * @param bytes (File size in bytes)
-   * @param decimals (Decimals point)
-   */
-  formatBytes(bytes, decimals) {
-    if (bytes === 0) {
-      return '0 Bytes';
-    }
-    const k = 1024;
-    const dm = decimals <= 0 ? 0 : decimals || 2;
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-    const i = Math.floor(Math.log(bytes) / Math.log(k));
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
-  }
 
   verifySign(){
 
